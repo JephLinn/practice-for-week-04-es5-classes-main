d69b90abfd310959a4a2b2e978e1bf2968a5caab
Jeffrey Manning <jeffmgpfit@yahoo.com>
2025-04-09 19:49:07 -0400

    Done

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,13 @@
-// Your code here
+Array.prototype.isEqual = function(arr) {
+    if (arr.length !== this.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== this[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
